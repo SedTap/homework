e2594ee87dc49a9c535fc2e0d1c7f13a5f45113e
SedTap <sedtap@mail.ru>
2026-05-25 15:59:09 +0300

    Add files via upload

---
+++ 10 Двадцатое ДЗ/main.js
@@ -0,0 +1,31 @@
+// Задача 1
+const firstName = "Egor";
+const lastName = "Polupanov";
+const isStudent = true;
+console.log(firstName, lastName, isStudent);
+
+
+// Задача 2
+const age = 19;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+console.log(birthYear);
+
+
+// Задача 3
+const infoStudent = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`;
+console.log(infoStudent);
+
+
+// Задача 4
+const result = "123456789falsetrue";
+console.log(result);
+
+
+
+
+
+
+
+
+
